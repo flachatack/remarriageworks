615cb4fb7fbfdc5d399404b85d7a29a9f0b08d89
tyler <trflach@yahoo.com>
2012-08-02 18:41:47 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RemarriageWorks::Application.config.secret_token = 'f3717397a2d558a8b10f4920c537b0d57b1f944d07b6368318c009477a2549bb2b278e75f45fc1f92d82d7baef755a05461695853f78a4e12bb1b429e698bbd4'
