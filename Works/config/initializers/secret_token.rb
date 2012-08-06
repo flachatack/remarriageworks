235bfda1980c22ced805fd3492f27a856d61b603
trflach <trflach@gmail.com>
2012-08-06 16:36:14 -0400

    removed extra app?

--- Works/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Works::Application.config.secret_token = 'ce287b7d45b495c8f0aaf42144a3e2c9ff947f51fa79f36852fcb8ced1114600d8d98ebf52507d7ac3a8a55e6f0e4c4fedccb5aebf06ee69a4c73142a7f2b506'
