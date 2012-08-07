58f9e2e5d1f557e1116adb244bbbc62dbd0a4d79
trflach <trflach@gmail.com>
2012-08-06 21:25:02 -0400

    production.erb changed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 RemarriageWorks::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-
+  config.assets.precompile += %w(  style.css  )
   # Code is not reloaded between requests
   config.cache_classes = true
 
