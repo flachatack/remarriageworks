dd6f30e3da267aeabd150a92dbda4457cb081f1e
trflach <trflach@gmail.com>
2012-08-15 16:30:11 -0400

    debug=false

--- config/application.rb
+++ config/application.rb
@@ -59,7 +59,7 @@ module RemarriageWorks
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    
+    config.assets.debug = false
     config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
